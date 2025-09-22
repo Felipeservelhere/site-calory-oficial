366fb0f1fd53c4cc68ead6b6accd2bfe36b0b318
Felipe servelhere <91691923+Felipeservelhere@users.noreply.github.com>
2025-09-22 15:55:58 -0300

    31313131313

--- callback/bling.php
+++ callback/bling.php
@@ -2,13 +2,14 @@
 $client_id     = '02d44294238e67e49277910a851a55a9395c838c6b8b530f163fad5d1200';
 $client_secret = '625ba407964f8db27c96283b104c991972abe5fb';
 $redirect_uri  = 'https://calory.com.br/callback/bling.php';
-$host = 'localhost';
-$db   = 'minhabase';
+$host = '177.107.115.204';
+$db   = 'integracao_bling';
 $user = 'root';
 $pass = '@@rOOt@cAlOry@1967@@';
+$port = 33060;
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
+    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$db;charset=utf8", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     $pdo->exec("CREATE TABLE IF NOT EXISTS bling_tokens (
